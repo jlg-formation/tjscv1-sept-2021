93d9458bdb3cdbd56f97620c0500157a3d8695b3
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-09-13 16:22:16 +0200

    dev: first hello server

--- back/server.js
+++ back/server.js
@@ -1,1 +1,13 @@
-console.log("starting the server...");
+const express = require("express");
+const serveIndex = require("serve-index");
+
+const app = express();
+const port = 3333;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
