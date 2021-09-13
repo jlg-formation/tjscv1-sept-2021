973e6a9b02eb9254aa6d231dfdcc317c36afca21
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-09-13 16:59:50 +0200

    dev: added first api

--- back/server.js
+++ back/server.js
@@ -9,6 +9,12 @@ app.use((req, res, next) => {
   next();
 });
 
+app.get("/api/date", (req, res) => {
+  res.json({
+    date: new Date(),
+  });
+});
+
 app.use(express.static("."));
 app.use(serveIndex("."));
 
