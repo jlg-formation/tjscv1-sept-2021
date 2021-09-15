5f8248df3ad26c1e0b2a51ff497478b20a8da9e5
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-09-15 10:46:51 +0200

    dev: added selected rows

--- front/src/routes/stock/List.tsx
+++ front/src/routes/stock/List.tsx
@@ -6,10 +6,17 @@ import "./List.scss";
 
 function List() {
   const [articles] = useState(articleHandler.articles);
+  const [selectedArticles, setSelectedArticles] = useState(new Set<Article>());
 
   function toggle(a: Article) {
     return (event: MouseEvent<HTMLElement>) => {
+      const newSelectedArticles = new Set(selectedArticles);
       console.log("toggle: ", a, event);
+      newSelectedArticles.has(a)
+        ? newSelectedArticles.delete(a)
+        : newSelectedArticles.add(a);
+
+      setSelectedArticles(newSelectedArticles);
     };
   }
 
@@ -33,7 +40,11 @@ function List() {
           </thead>
           <tbody>
             {articles.map((a) => (
-              <tr key={a.id} onClick={toggle(a)}>
+              <tr
+                key={a.id}
+                onClick={toggle(a)}
+                className={selectedArticles.has(a) ? "selected" : ""}
+              >
                 <td className="name">{a.name}</td>
                 <td className="price">{a.price} €</td>
                 <td className="qty">{a.qty}</td>
