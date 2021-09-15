5d19be5f1bc572002611984bf2ec6e4a9263b101
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-09-15 17:22:24 +0200

    test: adding one test more

--- front/src/App.test.tsx
+++ front/src/App.test.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Gérer efficacement/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("renders learn react link 2", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Voir le stock/i);
   expect(linkElement).toBeInTheDocument();
 });
