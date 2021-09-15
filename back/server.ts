7f3e00252dce1015c6ad0edcab5009198461bc31
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-09-15 14:33:16 +0200

    dev[back]: /api/articles running

--- back/server.ts
+++ back/server.ts
@@ -1,10 +1,16 @@
 import cors from "cors";
 import express from "express";
 import serveIndex from "serve-index";
+import { Article } from "./../front/src/interfaces/Article";
 
 const app = express();
 const port: number = 3333;
 const publicDir = "../front/build";
+
+const articles: Article[] = [
+  { id: "a1", name: "Tournevis xxx", price: 12.34, qty: 110 },
+  { id: "a2", name: "Tournevis cruciforme", price: 4.25, qty: 30 },
+];
 
 app.use(cors());
 
@@ -17,6 +23,10 @@ app.get("/api/date", (req, res) => {
   res.json({
     date: new Date(),
   });
+});
+
+app.get("/api/articles", (req, res) => {
+  res.json(articles);
 });
 
 app.use(express.static(publicDir));
